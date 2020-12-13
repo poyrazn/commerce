0deb85ef41cc8399ec6e1b3cbd21506d30178184
poyrazn <poyrazf@mef.edu.tr>
2020-12-13 06:00:45 +0300

    added product and watchlist

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Listing, Comment, User, Bid, SubCategory
+from .models import Category, Listing, Comment, User, Bid, SubCategory, Product, Watchlist
 # Register your models here.
 
 admin.site.register(Category)
@@ -9,3 +9,5 @@ admin.site.register(Comment)
 admin.site.register(User)
 admin.site.register(Bid)
 admin.site.register(SubCategory)
+admin.site.register(Product)
+admin.site.register(Watchlist)
