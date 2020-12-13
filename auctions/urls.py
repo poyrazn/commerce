8f083600eec1095366e8e4039241dbcea8efbd48
poyrazn <poyrazf@mef.edu.tr>
2020-12-13 06:03:02 +0300

    added watchlist

--- auctions/urls.py
+++ auctions/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("listings/<int:listing_id>", views.listing, name="listing"),
     path("watchlist", views.watchlist, name="watchlist"),
     path("create", views.create_listing, name="create_listing"),
-    path("watchlist/add", views.watchlist_add, name="watchlist_add"),
+    path("watchlist/add/listing<int:listing_id>", views.watchlist_add, name="watchlist_add"),
+    path("watchlist/remove/listing<int:listing_id>", views.watchlist_remove, name="watchlist_remove"),
     path("bid/listing<int:listing_id>", views.bid, name="bid"),
 ]
